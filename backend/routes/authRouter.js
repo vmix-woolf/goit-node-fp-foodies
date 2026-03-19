2b12396810a40d6fef4bbcf3d0876c9c00058cd8
Ivan Nazarov <nazarovv676@gmail.com>
2026-03-19 14:53:52 +0200

    fix(auth): add authenticate middleware to logout route

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { registerUser, loginUser, logoutUser } from "../controllers/authControllers.js";
 import { validateBody } from "../helpers/validateBody.js";
 import { registerSchema, loginSchema } from "../schemas/authSchemas.js";
+import authenticate from "../middleware/authenticate.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", validateBody(registerSchema), registerUser);
 authRouter.post("/login", validateBody(loginSchema), loginUser);
-authRouter.post("/logout", logoutUser); // need to add authenticate middleware
+authRouter.post("/logout", authenticate, logoutUser);
 
 export default authRouter;
